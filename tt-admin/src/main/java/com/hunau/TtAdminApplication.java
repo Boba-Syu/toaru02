a8e74235e622b7148a0e88a8f294805e693cfc71
Boba-Syu <1914808114@qq.com>
2019-04-07 20:17:55 +0800

    刚刚没推上去...

---
+++ tt-admin/src/main/java/com/hunau/TtAdminApplication.java
@@ -0,0 +1,13 @@
+package com.hunau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtAdminApplication.class, args);
+	}
+
+}
